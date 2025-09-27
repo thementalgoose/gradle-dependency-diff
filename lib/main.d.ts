843a1e89048117c4bde18e2f5ec2e97039058851
GitHub Action <action@github.com>
2025-09-27 16:02:29 +0000

    Release v2.0.0-alpha01

---
+++ lib/main.d.ts
@@ -0,0 +1,8 @@
+export interface Inputs {
+    before: string;
+    after: string;
+    outputToFile: boolean;
+    outputToFileName: string;
+    postPrComment: boolean;
+    repoToken: string;
+}
---
+++ lib/processor/output.d.ts
@@ -0,0 +1,2 @@
+import { DependencyTree } from '../models/dependency-tree.model';
+export declare function output(models: DependencyTree[], index?: number): string;
