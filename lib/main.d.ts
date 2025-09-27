3121896142103266076439274256e8f95eca8543
GitHub Action <action@github.com>
2025-09-27 13:13:41 +0000

    Release v0.1.0-alpha03

---
+++ lib/main.d.ts
@@ -0,0 +1,6 @@
+export interface Inputs {
+    before: string;
+    after: string;
+    outputToFile: boolean;
+    outputToFileName: string;
+}
---
+++ lib/utils/file.utils.d.ts
@@ -0,0 +1,2 @@
+export declare const readFile: (filename: string) => string[];
+export declare const writeFile: (filename: string, json: string) => boolean;
